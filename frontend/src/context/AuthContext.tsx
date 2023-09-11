81053ed1e3e0212602199afdf6d3ad1f0ef1139b
cboanta <m7df4nojhstijajgsykavsryfdnnqvhbzh7udaddmau6mdmixnka>
2023-09-11 20:34:40 +0300

    implemented conditional rendering of the user menu inside navbar component based on the global state (logged in/logged out)

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import { Action, AuthState, PropsWithChildren } from '../types';
 
 export const AuthContext = createContext({});
@@ -18,6 +18,15 @@ export const AuthContextProvider = ({children}: PropsWithChildren) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    useEffect(() => {
+        const userJson = localStorage.getItem('user');
+
+        if (userJson) {
+            const user = JSON.parse(userJson);
+            dispatch({ type: 'LOGIN', payload: user });
+        }
+    },[])
 
     console.log("AuthContext state:", state);
 
