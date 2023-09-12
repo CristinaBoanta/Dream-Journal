8dc5088fd7314eb0db35e109cd920fe459dbeda0
cboanta <cristina_boanta@proton.me>
2023-09-12 07:00:01 +0300

    created router redirects based on the user authentication state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,21 @@ import Navbar from "./components/Navbar";
 
 import "./index.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const App = () => {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
